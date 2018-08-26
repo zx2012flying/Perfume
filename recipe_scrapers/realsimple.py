d799dff5996ab263cb1b4a86cdc7ab55e632e9ac
zx2012flying <42462557+zx2012flying@users.noreply.github.com>
2018-08-26 15:53:23 +0800

    Update realsimple.py

--- recipe_scrapers/realsimple.py
+++ recipe_scrapers/realsimple.py
@@ -23,10 +23,10 @@ class RealSimple(AbstractScraper):
             {'class': "ingredients"}
         ).findAll('li')
 
-        return [
+        return '\n'.join([
             normalize_string(ingredient.get_text())
             for ingredient in ingredients
-        ]
+        ])
 
     def instructions(self):
         instructions = self.soup.findAll(
@@ -38,3 +38,12 @@ class RealSimple(AbstractScraper):
             normalize_string(instruction.find('p').get_text())
             for instruction in instructions
         ])
+    
+    def total_review(self):
+        total_review = self.soup.find('div',{'class': "total"}).get_text()
+        return re.findall("\d+",total_review)[0]  
+
+    def review_score(self):
+        review_score = self.soup.find('div', {'class': "rating"})
+        review_score = str(review_score)
+        return review_score.count('star on', 0, len(review_score))    
