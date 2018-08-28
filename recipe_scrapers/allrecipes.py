80a628fec20f82cd5aeb111aa30bdcbf23af4ab3
zx2012flying <42462557+zx2012flying@users.noreply.github.com>
2018-08-28 16:15:41 +0800

    Update allrecipes.py

--- recipe_scrapers/allrecipes.py
+++ recipe_scrapers/allrecipes.py
@@ -24,6 +24,15 @@ class AllRecipes(AbstractScraper):
             {'class': "checkList__line"}
         )
         
+        return '\n'.join([
+            normalize_string(ingredient.get_text())
+            for ingredient in ingredients
+            if ingredient.get_text(strip=True) not in (
+                'Add all ingredients to list',
+                '',
+                'ADVERTISEMENT'
+            )])
+    
         B = list()
         ING = [normalize_string(ingredient.get_text())
                for ingredient in ingredients
@@ -66,7 +75,6 @@ class AllRecipes(AbstractScraper):
                     i = ''.join([str(t), i])
                     break
             B.append(i)
-        return '\n'.join(B)
     
     def instructions(self):
         instructions = self.soup.findAll(
