fb088d5d7fa517c1bc53fa2c7020f9d4ec973177
zx2012flying <42462557+zx2012flying@users.noreply.github.com>
2018-08-28 16:06:09 +0800

    Add files via upload

---
+++ recipe_scrapers/dataprocess.py
@@ -0,0 +1,17 @@
+def convert_to_float(frac_str):
+    try:
+        return float(frac_str)
+    except ValueError:
+        try:
+            num, denom = frac_str.split('/')
+        except ValueError:
+            return None
+        try:
+            leading, num = num.split(' ')
+        except ValueError:
+            return float(num) / float(denom)        
+        if float(leading) < 0:
+            sign_mult = -1
+        else:
+            sign_mult = 1
+        return float(leading) + sign_mult * (float(num) / float(denom))
