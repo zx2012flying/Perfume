c0d1dfdd6897a2d1f65e9cffa3d246074afe0c8a
zx2012flying <42462557+zx2012flying@users.noreply.github.com>
2018-08-28 16:05:42 +0800

    Update allrecipes.py

--- recipe_scrapers/allrecipes.py
+++ recipe_scrapers/allrecipes.py
@@ -1,7 +1,7 @@
 import re
 from ._abstract import AbstractScraper
 from ._utils import get_minutes, normalize_string
-
+from .dataprocess import convert_to_float
 
 class AllRecipes(AbstractScraper):
 
@@ -23,17 +23,51 @@ class AllRecipes(AbstractScraper):
             'li',
             {'class': "checkList__line"}
         )
-
-        return '\n'.join([
-            normalize_string(ingredient.get_text())
-            for ingredient in ingredients
-            if ingredient.get_text(strip=True) not in (
-                'Add all ingredients to list',
-                '',
-                'ADVERTISEMENT'
-            )
-        ])
-
+        
+        B = list()
+        ING = [normalize_string(ingredient.get_text())
+               for ingredient in ingredients
+                    if ingredient.get_text(strip=True) not in (
+                        'Add all ingredients to list',
+                        '',
+                        'ADVERTISEMENT'
+                        )]
+        for i in A:
+            for j in i.split():
+                if convert_to_float(j) != None:
+                    t = convert_to_float(j)
+                    t = str(t)
+                    i = i.replace(j, t)
+      
+            t = float(0)        
+            for k in i.split():
+                try:
+                    t = t + float(k)
+                    i = i.strip(' ')
+                    i = i.strip(k)
+                except ValueError:
+                    i = ''.join([str(t), i])
+                    break
+ 
+            i = i.replace('cups', '237')
+            i = i.replace('cup', '237')
+            i = i.replace('teaspoons', '5')
+            i = i.replace('teaspoon', '5')
+            i = i.replace('tablespoons', '15')
+            i = i.replace('tablespoon', '15')
+                
+            t = float(1)        
+            for k in i.split():
+                try:
+                    t = t * float(k)
+                    i = i.strip(' ')
+                    i = i.strip(k)
+                except ValueError:
+                    i = ''.join([str(t), i])
+                    break
+            B.append(i)
+        return '\n'.join(B)
+    
     def instructions(self):
         instructions = self.soup.findAll(
             'span',
