21581688d98c7c878577ff06f01a7f7fb32bc23d
zx2012flying <42462557+zx2012flying@users.noreply.github.com>
2018-09-01 15:41:59 +0800

    Add files via upload

--- recipe_scrapers/geniuskitchen.py
+++ recipe_scrapers/geniuskitchen.py
@@ -1,7 +1,7 @@
 import re
 from ._abstract import AbstractScraper
 from ._utils import get_minutes, normalize_string
-
+from ._dataprocess import convert_to_float
 
 class GeniusKitchen(AbstractScraper):
 
@@ -23,19 +23,64 @@ class GeniusKitchen(AbstractScraper):
             'ul',
             {'class': 'ingredient-list'}
         ).findAll('li')
+                        
+        B = list()
+        ING = [normalize_string(ingredient.get_text())
+               for ingredient in ingredients
+                    if ingredient.get_text(strip=True) not in (
+                        'Add all ingredients to list',
+                        '',
+                        'ADVERTISEMENT'
+                        )]
 
-        return '\n'.join([
-            normalize_string(ingredient.get_text())
-            for ingredient in ingredients
-        ])
-
+        for i in ING:
+            i = i.replace('1⁄2', '1/2')
+            i = i.replace('1⁄3', '1/3')
+            i = i.replace('2⁄3', '2/3')
+            i = i.replace('1⁄4', '1/4')
+            i = i.replace('3⁄4', '3/4')            
+            for j in i.split():
+                if convert_to_float(j) != None:
+                    t = convert_to_float(j)
+                    t = str(t)
+                    i = i.replace(j, t, 1)
+      
+            t = float(0)        
+            for k in i.split():
+                try:
+                    t = t + float(k)
+                    i = i.strip(' ')
+                    i = i.strip(k)
+                except ValueError:
+                    i = ''.join([str(t), i])
+                    break
+ 
+            i = i.replace('cups', '237')
+            i = i.replace('cup', '237')
+            i = i.replace('teaspoons', '5')
+            i = i.replace('teaspoon', '5')
+            i = i.replace('tablespoons', '15')
+            i = i.replace('tablespoon', '15')
+                
+            t = float(1)        
+            for k in i.split():
+                try:
+                    t = t * float(k)
+                    i = i.strip(' ')
+                    i = i.strip(k)
+                except ValueError:
+                    i = ''.join([str(t), i])
+                    break
+            B.append(i)
+        return '\n'.join(B)
+    
     def instructions(self):
         instructions_html = self.soup.find(
             'div',
             {'class': 'directions-inner container-xs'}
         ).findAll('li')
 
-        return '\n'.join([
+        return '\n\n'.join([
             normalize_string(instruction.get_text())
             for instruction in instructions_html
         ])
