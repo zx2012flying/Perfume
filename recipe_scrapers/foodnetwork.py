c90cc06602cfc6ed9ad9d0febc56c03d0c737410
zx2012flying <42462557+zx2012flying@users.noreply.github.com>
2018-09-01 22:33:36 +0800

    Update foodnetwork.py

--- recipe_scrapers/foodnetwork.py
+++ recipe_scrapers/foodnetwork.py
@@ -1,6 +1,6 @@
 from ._abstract import AbstractScraper
 from ._utils import get_minutes, normalize_string
-
+from ._dataprocess import convert_to_float
 
 class FoodNetwork(AbstractScraper):
 
@@ -13,29 +13,83 @@ class FoodNetwork(AbstractScraper):
 
     def total_time(self):
         return get_minutes(self.soup.find(
-            'dd',
-            {'class': 'o-RecipeInfo__a-Description--Total'})
+            'span',
+            {'class': 'o-RecipeInfo__a-Description m-RecipeInfo__a-Description--Total'})
         )
 
     def ingredients(self):
         ingredients = self.soup.findAll(
+            'p',
+            {'class': 'o-Ingredients__a-Ingredient'}
+        )
+              
+        B = list()
+        ING = [normalize_string(ingredient.get_text())
+               for ingredient in ingredients
+                    if ingredient.get_text(strip=True) not in (
+                        'Add all ingredients to list',
+                        '',
+                        'ADVERTISEMENT'
+                        )]
+
+        for i in ING:
+            i = i.replace('1⁄2', '1/2')
+            i = i.replace('1⁄3', '1/3')
+            i = i.replace('2⁄3', '2/3')
+            i = i.replace('1⁄4', '1/4')
+            i = i.replace('3⁄4', '3/4')            
+            for j in i.split():
+                if convert_to_float(j) != None:
+                    t = convert_to_float(j)
+                    t = str(t)
+                    i = i.replace(j, t, 1)
+      
+            t = float(0)        
+            for k in i.split():
+                try:
+                    t = t + float(k)
+                    i = i.strip(' ')
+                    i = i.strip(k)
+                except ValueError:
+                    i = ''.join([str(t), i])
+                    break
+ 
+            i = i.replace('c.', '237')
+            i = i.replace('cups', '237')
+            i = i.replace('cup', '237')
+            i = i.replace('teaspoons', '5')
+            i = i.replace('teaspoon', '5')
+            i = i.replace('tsp.', '5')
+            i = i.replace('tablespoons', '15')
+            i = i.replace('tablespoon', '15')
+            i = i.replace('tbsp.', '15')
+                
+            t = float(1)        
+            for k in i.split():
+                try:
+                    t = t * float(k)
+                    i = i.strip(' ')
+                    i = i.strip(k)
+                except ValueError:
+                    i = ''.join([str(t), i])
+                    break
+            B.append(i)
+        return '\n'.join(B)
+    
+    def instructions(self):
+        instructions = self.soup.findAll(
             'li',
-            {'class': 'o-Ingredients__a-ListItem'}
+            {'class': 'o-Method__m-Step'}
         )
 
-        return [
-            normalize_string(ingredient.get_text())
-            for ingredient in ingredients
-        ]
-
-    def instructions(self):
-        instructions = self.soup.find(
-            'div',
-            {'class': 'o-Method__m-Body'}
-        ).findAll('p')
-
-        return '\n'.join([
+        return '\n\n'.join([
             normalize_string(instruction.get_text())
             for instruction in instructions
             if instruction.get_text(strip=True) not in ('Watch how to make this recipe.', )
         ])
+                        
+    def total_review(self):
+        return 0
+
+    def review_score(self):
+        return 0
