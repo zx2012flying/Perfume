ac213b3c323e98698e9a2fe0a106d21328b64680
zx2012flying <42462557+zx2012flying@users.noreply.github.com>
2018-09-07 12:08:02 +0800

    Add files via upload

---
+++ recipe_scrapers/yummly.py
@@ -0,0 +1,94 @@
+import re
+from ._abstract import AbstractScraper
+from ._utils import get_minutes, normalize_string
+from ._dataprocess import convert_to_float
+
+class AllRecipes(AbstractScraper):
+
+    @classmethod
+    def host(self):
+        return 'yummly.com'
+
+    def title(self):
+        return self.soup.find('h1').get_text()
+
+    def total_time(self):
+        return get_minutes(self.soup.find(
+            'div',
+            {'class': 'ready-in-time'})
+        )
+
+    def ingredients(self):
+        ingredients = self.soup.findAll(
+            'li',
+            {'class': "IngredientLine"}
+        )
+        
+        B = list()
+        
+        ING = [normalize_string(ingredient.get_text())
+               for ingredient in ingredients
+                    if ingredient.get_text(strip=True) not in (
+                        'Add all ingredients to list',
+                        '',
+                        'ADVERTISEMENT'
+                        )]
+        
+        for i in ING:
+            for j in i.split():
+                if convert_to_float(j) != None:
+                    t = convert_to_float(j)
+                    t = str(t)
+                    i = i.replace(j, t, 1)
+      
+            t = float(0)        
+            for k in i.split():
+                try:
+                    t = t + float(k)
+                    i = i.strip(' ')
+                    i = i.strip(k)
+                except ValueError:
+                    i = ''.join([str(t), i])
+                    break
+ 
+            i = i.replace('cups', '237')
+            i = i.replace('cup', '237')
+            i = i.replace('teaspoons', '5')
+            i = i.replace('teaspoon', '5')
+            i = i.replace('tablespoons', '15')
+            i = i.replace('tablespoon', '15')
+                
+            t = float(1)        
+            for k in i.split():
+                try:
+                    t = t * float(k)
+                    i = i.strip(' ')
+                    i = i.strip(k)
+                except ValueError:
+                    i = ''.join([str(t), i])
+                    break
+            B.append(i)
+        return '\n'.join(B)
+
+    
+    def instructions(self):
+        instructions = self.soup.findAll(
+            'span',
+            {'class': 'recipe-directions__list--item'}
+        )
+
+        return '\n\n'.join([
+            normalize_string(instruction.get_text())
+            for instruction in instructions
+        ])
+
+    def total_review(self):
+        TR = self.soup.find(
+                'div',
+                {'id': "reviews"}, {'class', 'reviews wrapper'}
+                ).get_text('span', {'class': 'font-number'})
+        tr = re.findall("\d+",TR)[0]
+        return tr
+    
+    def review_score(self):        
+        return 0
